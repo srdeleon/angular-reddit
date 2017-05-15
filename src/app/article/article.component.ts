ee04e16b065343f444d0c29a8ed48efeea2bbe85
srdeleon <silvio.deleon@gmail.com>
2017-05-14 23:52:45 -0400

    Initial commit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,33 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+	@HostBinding('attr.class') cssClass = 'row';
+	article: Article;
+
+  constructor() {
+  	this.article = new Article(
+  		'Angular 2',
+  		'http://angular.io',
+  		10);
+  }
+
+  voteUp() {
+  	this.article.voteUp();
+  	return false;
+  }
+
+  voteDown() {
+  	this.article.voteDown();
+  	return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
