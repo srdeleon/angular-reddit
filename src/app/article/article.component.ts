5dd95d2d5b1a7c39de4fb88af2ed2b7472db8269
Silvio de León <silvio_de_leon@claro.com.do>
2017-05-15 18:12:49 -0400

    Completed angular-reddit app

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -7,24 +7,21 @@ import { Article } from './article.model';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-	@HostBinding('attr.class') cssClass = 'row';
-	article: Article;
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
 
   constructor() {
-  	this.article = new Article(
-  		'Angular 2',
-  		'http://angular.io',
-  		10);
+
   }
 
   voteUp() {
-  	this.article.voteUp();
-  	return false;
+    this.article.voteUp();
+    return false;
   }
 
   voteDown() {
-  	this.article.voteDown();
-  	return false;
+    this.article.voteDown();
+    return false;
   }
 
   ngOnInit() {
